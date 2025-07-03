f7c093b7abf9c854c80c9c210035b06333381fd7
Dhruv Bhanushali <dhruv_b@live.com>
2025-07-03 14:51:50 +0400

    Enable required QMK features

---
+++ keyboards/zsa/moonlander/keymaps/supreme/rules.mk
@@ -0,0 +1,14 @@
+# ZSA rules
+ORYX_ENABLE = yes
+
+# RGB Matrix: https://docs.qmk.fm/features/rgb_matrix#custom-rgb-matrix-effects
+RGB_MATRIX_CUSTOM_KB = yes
+
+# Auto Shift: https://docs.qmk.fm/features/auto_shift
+AUTO_SHIFT_ENABLE = yes
+
+# Tap Dance: https://docs.qmk.fm/features/tap_dance
+TAP_DANCE_ENABLE = yes
+
+# Caps Word: https://docs.qmk.fm/features/caps_word
+CAPS_WORD_ENABLE = yes
