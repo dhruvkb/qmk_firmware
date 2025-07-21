29abad78f5eb0ef0f20d5fccf2cf3c400406b6e0
Dhruv Bhanushali <dhruv_b@live.com>
2025-07-22 05:24:13 +0530

    Replace paired keys with just left of each

--- keyboards/zsa/moonlander/keymaps/supreme/keymap.c
+++ keyboards/zsa/moonlander/keymaps/supreme/keymap.c
@@ -289,10 +289,7 @@ enum custom_keycodes {
     MOD_OFF,
     PW_OS,
     PW_1P,
-    BR_ROUND,
-    BR_SQUAR,
-    BR_ANGLE,
-    BR_CURLY,
+    KC_LSQR,
 };
 
 // Handle custom key presses.
@@ -328,24 +325,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 SEND_STRING(SS_TAP(X_ENTER));
             }
             return false;
-        case BR_ROUND:
+        case KC_LSQR:
             if (record->event.pressed) {
-                SEND_STRING("()" SS_TAP(X_LEFT));
-            }
-            return false;
-        case BR_SQUAR:
-            if (record->event.pressed) {
-                SEND_STRING("[]" SS_TAP(X_LEFT));
-            }
-            return false;
-        case BR_ANGLE:
-            if (record->event.pressed) {
-                SEND_STRING("<>" SS_TAP(X_LEFT));
-            }
-            return false;
-        case BR_CURLY:
-            if (record->event.pressed) {
-                SEND_STRING("{}" SS_TAP(X_LEFT));
+                // Send the left square bracket character.
+                SEND_STRING("[");
             }
             return false;
     }
@@ -379,7 +362,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,             KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,          MOD_OFF,       /* row 2 */ KC_EQL,        KC_Y,          KC_U,          KC_I,          KC_O,          KC_P,          KC_BSLS,
         CW_TOGG,            KC_A,          KC_S,          KC_D,          KC_F,          KC_G,          KC_ESC,        /* row 3 */ KC_DEL,        KC_H,          KC_J,          KC_K,          KC_L,          KC_SCLN,       KC_QUOT,
         OSM(MOD_LSFT),      KC_Z,          KC_X,          KC_C,          KC_V,          KC_B,                         /* row 4 */                KC_N,          KC_M,          KC_COMM,       KC_DOT,        KC_UP,         KC_ENT,
-        OSM(MOD_LALT),      BR_ROUND,      BR_CURLY,      BR_SQUAR,      BR_ANGLE,      /* orange → */ RAYCAST,       /* row 5 */ WIN_MGMT,      /* ← orange */ OSM(MOD_LGUI), KC_SLSH,       KC_LEFT,       KC_DOWN,       KC_RIGHT,
+        OSM(MOD_LALT),      KC_LPRN,       KC_LCBR,       KC_LSQR,       KC_LABK,       /* orange → */ RAYCAST,       /* row 5 */ WIN_MGMT,      /* ← orange */ OSM(MOD_LGUI), KC_SLSH,       KC_LEFT,       KC_DOWN,       KC_RIGHT,
                                                                          KC_SPC,        OSM(MOD_LGUI), OSM(MOD_LCTL), /* thumb */ OSM(MOD_LALT), OSM(MOD_LSFT), KC_BSPC
     ),
     [1] = LAYOUT_moonlander(
