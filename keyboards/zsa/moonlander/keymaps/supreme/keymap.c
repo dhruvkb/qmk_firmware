53786f68b980b9565a39f6b64ea38a2a7cdf08ad
Dhruv Bhanushali <dhruv_b@live.com>
2026-08-12 19:49:36 +0400

    Rearrange base layer keys

--- keyboards/zsa/moonlander/keymaps/supreme/keymap.c
+++ keyboards/zsa/moonlander/keymaps/supreme/keymap.c
@@ -428,12 +428,12 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 // TODO: Update keycodes for mouse keys when ZSA's fork of QMK is updated.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_moonlander(
-        KC_GRV,             KC_1,          KC_2,          KC_3,          KC_4,          KC_5,          TD(LAYER_1),   /* row 1 */ TD(LAYER_2),   KC_6,          KC_7,          KC_8,          KC_9,          KC_0,          KC_MINS,
-        KC_TAB,             KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,          MOD_OFF,       /* row 2 */ KC_EQL,        KC_Y,          KC_U,          KC_I,          KC_O,          KC_P,          KC_BSLS,
-        CW_TOGG,            KC_A,          KC_S,          KC_D,          KC_F,          KC_G,          KC_ESC,        /* row 3 */ KC_DEL,        KC_H,          KC_J,          KC_K,          KC_L,          KC_SCLN,       KC_QUOT,
-        OSM(MOD_LSFT),      KC_Z,          KC_X,          KC_C,          KC_V,          KC_B,                         /* row 4 */                KC_N,          KC_M,          KC_COMM,       KC_DOT,        KC_UP,         KC_ENT,
-        OSM(MOD_LALT),      TD(PRN),       TD(CBR),       TD(BRC),       TD(ABK),       /* orange → */ RAYCAST,       /* row 5 */ WIN_MGMT,      /* ← orange */ OSM(MOD_LGUI), KC_SLSH,       KC_LEFT,       KC_DOWN,       KC_RIGHT,
-                                                                         KC_SPC,        OSM(MOD_LGUI), OSM(MOD_LCTL), /* thumb */ OSM(MOD_LALT), OSM(MOD_LSFT), KC_BSPC
+        KC_GRV,        KC_1,          KC_2,          KC_3,          KC_4,          KC_5,          TD(LAYER_3),   /* row 1 */ XXXXXXX,       KC_6,          KC_7,          KC_8,          KC_9,           KC_0,          KC_MINS,
+        KC_TAB,        KC_Q,          KC_W,          KC_E,          KC_R,          KC_T,          MOD_OFF,       /* row 2 */ KC_EQL,        KC_Y,          KC_U,          KC_I,          KC_O,           KC_P,          KC_BSLS,
+        CW_TOGG,       KC_A,          KC_S,          KC_D,          KC_F,          KC_G,          KC_ESC,        /* row 3 */ KC_DEL,        KC_H,          KC_J,          KC_K,          KC_L,           KC_SCLN,       KC_QUOT,
+        OSM(MOD_LSFT), KC_Z,          KC_X,          KC_C,          KC_V,          KC_B,                         /* row 4 */                KC_N,          KC_M,          KC_COMM,       KC_DOT,         KC_UP,         KC_ENT,
+        OSM(MOD_LALT), KC_LBRC,       KC_RBRC,       XXXXXXX,       TD(LAYER_1),   /* orange → */ RAYCAST,       /* row 5 */ WIN_MNG,       /* ← orange */ TD(LAYER_2),   KC_SLSH,       KC_LEFT,        KC_DOWN,       KC_RGHT,
+                                                                    KC_SPACE,      OSM(MOD_LGUI), OSM(MOD_LCTL), /* thumb */ OSM(MOD_LALT), OSM(MOD_LSFT), KC_BSPC
     ),
     [1] = LAYOUT_moonlander(
         _______,            KC_F1,         KC_F2,         KC_F3,         KC_F4,         KC_F5,         TO(0),         /* row 1 */ TO(0),         KC_F6,         KC_F7,         KC_F8,         KC_F9,         KC_F10,        _______,
